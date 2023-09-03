c88ded24ce029b24de08968ba84cb073b22576dc
Avyukth <makur.subhrajit@gmail.com>
2023-09-04 02:44:37 +0530

    dockerignore added

--- stores/stores_test.go
+++ stores/stores_test.go
@@ -1,6 +1,6 @@
 package stores
 
-import(
+import (
 	"fmt"
 	"github.com/cucumber/godog"
 )
@@ -8,8 +8,8 @@ import(
 var storeName = ""
 
 func aStoreCalledExists(name string) error {
-	if storeName!= name {
-        return fmt.Errorf("store does not exist: %s", name)
+	if storeName != name {
+		return fmt.Errorf("store does not exist: %s", name)
 	}
 	return nil
 }
@@ -20,20 +20,19 @@ func aValidStoreOwner() error {
 
 func iCreateTheStoreCalled(name string) error {
 	storeName = name
-    return nil
+	return nil
 }
 
 func noStoreCalledExists(name string) error {
 	if storeName == name {
-        return fmt.Errorf("store already exists: %s", name)
-    }
-    return nil
+		return fmt.Errorf("store already exists: %s", name)
+	}
+	return nil
 }
-
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^a store called "([^"]*)" exists$`, aStoreCalledExists)
-    ctx.Step(`^a valid store owner$`, aValidStoreOwner)
-    ctx.Step(`^I create the store called "([^"]*)"$`, iCreateTheStoreCalled)
-    ctx.Step(`^no store called "([^"]*)" exists$`, noStoreCalledExists)
+	ctx.Step(`^a valid store owner$`, aValidStoreOwner)
+	ctx.Step(`^I create the store called "([^"]*)"$`, iCreateTheStoreCalled)
+	ctx.Step(`^no store called "([^"]*)" exists$`, noStoreCalledExists)
 }
